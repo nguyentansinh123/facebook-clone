4179ef81ec9b977f326ee0dadfce80e1a0fbe004
nguyentansinh123 <66555356+nguyentansinh123@users.noreply.github.com>
2021-08-31 23:53:21 +0700

    Update config.js

--- src/components/firebase/config.js
+++ src/components/firebase/config.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 const firebaseConfig = {
-    apiKey: "AIzaSyCJRMsor1Kdp9zclZ4j3oQt7WvwV-dqS3s",
-    authDomain: "facebook-clone-react-a2bf2.firebaseapp.com",
-    projectId: "facebook-clone-react-a2bf2",
-    storageBucket: "facebook-clone-react-a2bf2.appspot.com",
-    messagingSenderId: "988073215838",
-    appId: "1:988073215838:web:da0ae68ac094badf4eb1cf",
-    measurementId: "G-JQYE7S1D9W"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
